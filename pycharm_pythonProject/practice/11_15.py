603f192ea6c52dd1b6f2a42680fe7f201c2e962a
cjh <1103774337@qq.com>
2025-11-26 20:27:32 +0800

    study for 11_26

--- pycharm_pythonProject/practice/11_15.py
+++
@@ -1,9 +0,0 @@
-# n = 18
-# print(n << (n & 1))
-
-# n = 3
-# hmap = dict[int, str]()
-# for i in range(n):
-#     hmap[i] = str(i)
-# print(hmap)
-
---
+++ pycharm_pythonProject/practice/homework/finance_manager.py
@@ -0,0 +1,39 @@
+class FinanceManager:
+    def __int__(self, amount,category, ):
+        self.amount = amount
+
+        pass
+
+
+    def add_transaction(amount, category, description):
+        """用于管理个人收入和支出"""
+
+        pass
+
+
+    def get_balance():
+        """添加交易记录"""
+
+        pass
+
+    def get_balance():
+        """计算并返回当前余额"""
+
+        pass
+
+    def get_summary_by_category():
+        """按类别汇总交易金额"""
+
+        pass
+
+
+    def save_to_file(filename):
+        """用于数据持久化"""
+
+        pass
+
+
+    def load_from_file(filename):
+        """用于数据持久化"""
+
+        pass

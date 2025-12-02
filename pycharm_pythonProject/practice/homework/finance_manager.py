e06152d28a419e36359303c8c612fcd8091f36ee
cjh <1103774337@qq.com>
2025-12-02 20:48:35 +0800

    study for 11_30

--- pycharm_pythonProject/practice/homework/finance_manager.py
+++ pycharm_pythonProject/practice/homework/finance_manager.py
@@ -1,5 +1,7 @@
+# from pathlib import Path
+
 class FinanceManager:
-    def __init__(self, cont):
+    def __init__(self):
         self.cont = []
         pass
 
@@ -8,42 +10,52 @@ class FinanceManager:
         dict_cunt = {"金额": amount,
                      "类别": category,
                      "描述": description,
-                     "type":"收入" if amount > 0 else "支出"
+                     "type": "收入" if amount > 0 else "支出"
                      }
         self.cont.append(dict_cunt)
-        type = "收入" if amount > 0 else "支出"
-        return f"{type}添加成功！"
+        get_type = "收入" if amount > 0 else "支出"
+        return f"{get_type}添加成功！"
 
     def get_balance(self):
         """计算并返回当前余额"""
         result = sum(item["金额"] for item in self.cont)
         return result
 
-    def get_summary_by_category(self):
+    def get_summary_by_category(self, category):
         """按类别汇总交易金额"""
         list_cont = []
         for i in self.cont:
-            if i["类别"] not in list_cont:
-                dict_cont["类别"] = i["类别"]
-                dict_cont["金额"] = i["金额"]
-            else:
-                dict_cont["金额"] += i["金额"]
-        return dict_cont
+            if i["类别"] == category:
+                dict_cunt = {"金额": i["金额"],
+                             "描述": i["描述"]
+                             }
+                list_cont.append(dict_cunt)
+        # print(self.cont)
+        # print(list_cont)
+        return list_cont
 
-    def save_to_file(self, filename):
-        """用于数据持久化"""
+    # def save_to_file(self, filename):
+    #     """用于数据持久化"""
+    #     # 创建文件夹
+    #     folder = Path("new_folder")
+    #     folder.mkdir()
+    #
+    #     # 写入文件
+    #     file_path = folder.joinpath("FinanceManger.txt")
+    #     with open(file_path, "w") as file:
+    #         file.write(self.cont)
+    #     pass
+    #
+    # def load_from_file(self):
+    #     """用于数据持久化"""
+    #
+    #     pass
 
-        pass
 
-    def load_from_file(self):
-        """用于数据持久化"""
-
-        pass
-
-
-def main():
+if __name__ == "__main__":
+    manager = FinanceManager()
     while True:
-        print("1. 添加收入 2. 添加支出 3. 查看余额 4. 查看分类汇总 5. 保存数据 6. 加载数据 7. 退出")
+        print("\n\n1. 添加收入 2. 添加支出 3. 查看余额 4. 查看分类汇总 5. 保存数据 6. 加载数据 7. 退出")
         print("请选择: ")
         choose = input().strip()
         if choose == "1":
@@ -54,9 +66,8 @@ def main():
                 continue
             category = input("请输入类别：")
             description = input("请输入描述：")
-            FinanceManager(amount=amount, category=category, description=description)
-            print("收入添加成功！")
-
+            end = manager.add_transaction(amount=amount, category=category, description=description)
+            print(end)
         elif choose == "2":
             try:
                 amount = float(input("请输入金额：").strip())
@@ -68,16 +79,27 @@ def main():
             else:
                 category = input("请输入类别：")
                 description = input("请输入描述：")
-
-                print("支出添加成功！")
+                end = manager.add_transaction(amount=amount, category=category, description=description)
+                print(end)
         elif choose == "3":
+            end = manager.get_balance()
+            print(f"余额为：{end}")
             pass
         elif choose == "4":
+            category = input("请输入类别：")
+            end = manager.get_summary_by_category(category)
+            if len(end) == 0:
+                print("没有该类型！")
+                continue
+            for i in end:
+                print(f"类别：{category} 金额：{i['金额']} 描述:{i['描述']}")
             pass
-        elif choose == "5":
-            pass
-        elif choose == "6":
-            pass
+        # elif choose == "5":
+        #
+        #     pass
+        # elif choose == "6":
+        #
+        #     pass
         elif choose == "7":
             exit()
         else:
